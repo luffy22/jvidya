e87f9350c387a6743cb89b4d3888252a6c83fe44
luffy22 <kopnite@gmail.com>
2014-02-16 06:30:20 +0530

    home icon new changes 2

--- templates/jyotishi_vidya/index.php
+++ templates/jyotishi_vidya/index.php
@@ -11,6 +11,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <jdoc:include type="head" />
+<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/media/jui/css/bootstrap.css" />
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/media/jui/css/bootstrap-responsive.css"/>
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
@@ -23,10 +24,10 @@
     <div class="header-inner">
         <a href="index.php"><img src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/images/jv_logo.png" alt="Jyotishi Vidya" title="Navigate to Home Page" height="150" width="400" /></a>    
     </div>
-    <div class="header-menu">
-        <ul class="nav nav-pills">
-            <li>
-                <a href="index.php"><img src="<?php echo $this->baseurl; ?>/images/home_icon.jpg" alt="Jyotishi Vidya" title="Navigate to Home Page" height="30px" width="30px" /></a>
+    <div class="header-menu visible-desktop">
+        <ul class="nav nav-pills visible-desktop">
+            <li class="home_icon">
+                <a href="index.php"><img src="<?php echo $this->baseurl; ?>/images/home_logo.png" alt="Jyotishi Vidya" title="Navigate to Home Page" width="35px" height="35px" /></a>
             </li>
             <li class="dropdown">
                 <jdoc:include type="modules" name="menu" style="none" />
@@ -51,7 +52,7 @@
 <!-- The main body container -->
 <div class="main-section">
     <div class ="container-fluid">
-    <div class="span3">
+    <div class="span3 visible-desktop">
         <jdoc:include type="modules" name="userlogin" style="none" />
     </div>
     <div class="span6">
@@ -59,7 +60,7 @@
         <jdoc:include type="component" />
         <jdoc:include type="message" />
     </div>
-     <div class="span3">
+     <div class="span3 visible-desktop">
         <jdoc:include type="modules" name="socialplugins" style="none" />
     </div>
 </div>
