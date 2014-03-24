e036dfe2527a4f7690155a012c624d5bf67f79a3
luffy22 <kopnite@gmail.com>
2014-03-24 09:19:52 +0530

    social network plugin & template design changed

--- modules/mod_socialfollow/helper.php
+++ modules/mod_socialfollow/helper.php
@@ -3,7 +3,6 @@
 /*
  * Helper class for Menu1 Module
  */
-
 class modSocialHelper
 {
 
@@ -13,7 +12,7 @@ class modSocialHelper
         <table class="table-condensed">
             <tr>
                 <td><div class="fb-follow" data-href="https://www.facebook.com/jyotishividya" data-width="20px" data-height="20px" data-colorscheme="light" data-layout="button" data-show-faces="false"></div></td>
-                <td><div class="g-follow" data-annotation="none" data-height="20" data-href="//plus.google.com/100464003715258637571" data-rel="publisher"></div></td>
+                <td><div class="g-follow" data-annotation="none" data-height="20" data-href="https://plus.google.com/+JyotishividyaCoInplus" data-rel="publisher"></div></td>
                 <td><a href="https://twitter.com/JyotishiVidya" class="twitter-follow-button" data-show-count="false" data-size="medium" data-show-screen-name="false">Follow</a></td>
             </tr>
         </table>
