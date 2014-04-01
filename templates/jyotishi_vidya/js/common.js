71c86e018bac9ec5c403e02f2e5c3fbb6c351f7c
luffy22 <kopnite@gmail.com>
2014-04-01 10:15:45 +0530

    slider banner module changed

--- templates/jyotishi_vidya/js/common.js
+++ templates/jyotishi_vidya/js/common.js
@@ -85,8 +85,18 @@ jQuery('#left_menu').click(function()
     }
 }*/
 
-function slideBanner(url)
+function slideBanner()
 {
-    var folderpath = url;
-    jQuery('');
+    alert("calls");
+jQuery("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+jQuery('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
+
 }
