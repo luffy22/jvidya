ed87e789ee5b4dc25e96541630b023a7fcd3520f
luffy22 <kopnite@gmail.com>
2014-06-30 16:57:22 +0530

    new module nakshatra compat added

---
+++ modules/mod_nakshatracompat/helper.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_login
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+define('_JEXEC', 1);
+
+/**
+ * Helper for mod_login
+ *
+ * @package     Joomla.Site
+ * @subpackage  mod_login
+ * @since       1.5
+ */
+class modNakshatraCompatHelper
+{
+    public function showMessage()
+    {
+		echo "Hello World";
+	}
+}
---
+++ modules/mod_nakshatracompat/mod_nakshatracompat.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_login
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+// Include the login functions only once
+require_once __DIR__ . '/helper.php';
+
+
+$msg	          = modNakshatraCompatHelper::showMessage();
+
+
+require JModuleHelper::getLayoutPath('mod_nakshatracompat', $layout);
